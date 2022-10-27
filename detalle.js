28a50a69419e17c29b5ceae37d34d5de3f0933ce
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-10-27 15:49:49 -0300

    cambios de estilos

--- detalle.js
+++ detalle.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('button');
 const contenedor = document.querySelector('#app');
+const titulo = document.querySelector('h1');
 const servidor = 'https://reqres.in/api/'
 let url = location.search;
 let id = location.search.split('=')[1];
@@ -21,6 +22,21 @@ btn.addEventListener('click', ()=>{
     .then( respuestaJSON => {
         console.log(respuestaJSON.data);
         let datos = respuestaJSON.data;
+
+        renderizar(datos);
     })
 
 
+function renderizar(user){
+        console.log(user)
+        let html = '';
+        titulo.innerHTML = `${user.first_name}</strong> ${user.last_name}`;
+        html += `<div class="card-2">
+                        <h4><strong> ${user.first_name}</strong> ${user.last_name}</h4>
+                        <img src="${user.avatar}" alt="${user.first_name}">
+
+                        <p>${user.email}</p>
+                    </div>`;
+        
+        contenedor.innerHTML = html;
+}
